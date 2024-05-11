e25c54be9471b7013593acc6d89efebc67846176
jontopielski <polkorame@gmail.com>
2024-05-11 09:41:31 -0700

    Final desktop pet push

--- src/main.gd
+++ src/main.gd
@@ -1,54 +1,130 @@
 extends Node2D
 
+@export var require_folder: bool = true
+
+enum Wall { LEFT, TOP, RIGHT, BOTTOM }
+
 const Pet = preload("res://src/pet.tscn")
-const DEMON_FOLDER = "demon"
-const TEXT_FILE = "notes.txt"
+const DEMON_FOLDER = "init"
+const TEXT_FILE = "Readme.txt"
 
 var is_summoned = false
 var pet = null
+var walls_touched = []
+var last_window_pos = Vector2.ZERO
 
 func _ready():
 	get_window().position = Vector2i(1, 1)
 	get_window().size = DisplayServer.screen_get_size() - Vector2i(2, 2)
-	get_window().mouse_passthrough_polygon = PackedVector2Array([Vector2(0, 0), Vector2(0, 1)])
+	update_instructions_passthrough()
+	last_window_pos = $Instructions.position
+	if !require_folder or has_demon_folder(get_desktop_file_names()):
+		$Instructions.queue_free()
+		$FileCheckTimer.stop()
+		summon_pet()
+		await get_tree().create_timer(0.01).timeout
+		tween_summon_animation()
+
+func update_instructions_passthrough():
+	var mult = Vector2(get_window().size) / get_viewport_rect().size
+	var pos = Vector2($Instructions.position) - Vector2(10, 30)
+	var size = Vector2($Instructions.size) + Vector2(20, 40)
+	get_window().mouse_passthrough_polygon = PackedVector2Array([pos * mult, (pos + Vector2(size.x, 0)) * mult,
+		(pos + size) * mult, (pos + Vector2(0, size.y)) * mult])
+
+func _process(_delta):
+	if has_node("Instructions") and $Instructions.position != last_window_pos:
+		last_window_pos = $Instructions.position
+		update_instructions_passthrough()
+	if touched_wall(Wall.TOP) and !Wall.TOP in walls_touched:
+		walls_touched.push_back(Wall.TOP)
+	if touched_wall(Wall.BOTTOM) and !Wall.BOTTOM in walls_touched:
+		walls_touched.push_back(Wall.BOTTOM)
+	if touched_wall(Wall.LEFT) and !Wall.LEFT in walls_touched:
+		walls_touched.push_back(Wall.LEFT)
+	if touched_wall(Wall.RIGHT) and !Wall.RIGHT in walls_touched:
+		walls_touched.push_back(Wall.RIGHT)
+	if !walls_touched.is_empty() and $TouchedWallsTimer.is_stopped():
+		$TouchedWallsTimer.start()
+	if len(walls_touched) == 4 and !$TouchedWallsTimer.is_stopped():
+		walls_touched.clear()
+		$TouchedWallsTimer.stop()
+		if pet:
+			pet.toggle_inverted()
+
+func touched_wall(wall):
+	match wall:
+		Wall.TOP:
+			return get_global_mouse_position().y <= 0.0
+		Wall.BOTTOM:
+			return get_global_mouse_position().y >= 360.0
+		Wall.LEFT:
+			return get_global_mouse_position().x <= 0.0
+		Wall.RIGHT:
+			return get_global_mouse_position().x >= 640.0
+	return false
 
 func handle_desktop_folder_check():
 	var desktop_files = get_desktop_file_names()
-	if DEMON_FOLDER in desktop_files:
+	if has_demon_folder(desktop_files):
 		$FileCheckTimer.stop()
 		is_summoned = true
-		await get_tree().create_timer(1.5).timeout
+		await get_tree().create_timer(0.5).timeout
+		if has_node("Instructions"):
+			$Instructions.queue_free()
+		await get_tree().create_timer(0.5).timeout
 		summon_pet()
 		write_text_file()
 		await get_tree().create_timer(0.01).timeout
 		tween_summon_animation()
 
+func has_demon_folder(files):
+	for file in files:
+		if file.to_lower() == DEMON_FOLDER:
+			return true
+	return false
+
 func tween_summon_animation():
 	var scale_tween = get_tree().create_tween()
-	pet.scale_multiplier = 5.0
-	scale_tween.tween_property(pet, "scale_multiplier", 1, 1.0).set_trans(Tween.TRANS_SPRING)
+	pet.scale_multiplier = 2.0
+	scale_tween.tween_property(pet, "scale_multiplier", 1, 0.5).set_trans(Tween.TRANS_SPRING)
 	var position_tween = get_tree().create_tween()
-	position_tween.tween_property(pet, "position", get_viewport_rect().size / 2.0, 1.0).set_trans(Tween.TRANS_SPRING)
+	position_tween.tween_property(pet, "position", get_viewport_rect().size / 2.0, 0.5).set_trans(Tween.TRANS_SPRING)
 
 func summon_pet():
 	pet = Pet.instantiate()
 	add_child(pet)
-	pet.position = get_viewport_rect().size / 2.0 + Vector2(0, get_viewport_rect().size.y / 8)
+	pet.position = get_viewport_rect().size / 2.0 + Vector2(0, get_viewport_rect().size.y / 16)
 	pet.pet_changed.connect(_on_pet_changed)
 	update_passthrough_polygon()
 
 func write_text_file():
-	var file = FileAccess.open(OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP) + "/%s/%s" % [DEMON_FOLDER, TEXT_FILE], FileAccess.WRITE)
+	var file = FileAccess.open(OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP) + "/%s/%s" % [get_demon_file_name(), TEXT_FILE], FileAccess.WRITE)
 	if !file:
 		return
-	file.store_line("Hello!")
-	file.store_line("My name is Atgtha")
-	file.store_line("I'm your demon pet >:)")
-	file.store_line("--------")
-	file.store_line("Pet me")
-	file.store_line("Love me")
-	file.store_line("But most of all, love yourself!")
+	file.store_line("Congrats on summoning your very own Desktop Daemon!")
+	file.store_line("")
+	file.store_line("v1.0")
+	file.store_line("")
+	file.store_line("How to use:")
+	file.store_line("- Watch the daemon wander around")
+	file.store_line("- Click on the daemon to pet it")
+	file.store_line("- Click and hold to drag it around")
+	file.store_line("- Right click to exit")
+	file.store_line("")
+	file.store_line("Credits:")
+	file.store_line("- Prifurin (Art, Design)")
+	file.store_line("- Jon Topielski (Programming, Design)")
+	file.store_line("")
+	file.store_line("Made with Godot")
 	file.close()
+
+func get_demon_file_name():
+	var desktop_files = get_desktop_file_names()
+	for file in desktop_files:
+		if file.to_lower() == DEMON_FOLDER:
+			return file
+	return ""
 
 func get_desktop_file_names():
 	var files = []
@@ -79,3 +155,18 @@ func _on_pet_changed():
 func _on_file_check_timer_timeout():
 	if !is_summoned:
 		handle_desktop_folder_check()
+
+func _on_instructions_close_requested():
+	$Instructions.queue_free()
+	await get_tree().create_timer(0.05).timeout
+	if !is_summoned:
+		get_window().mouse_passthrough_polygon = PackedVector2Array([Vector2(0, 0), Vector2(0, 1)])
+
+func _on_touched_walls_timer_timeout():
+	walls_touched.clear()
+
+func _on_button_pressed():
+	$Instructions.queue_free()
+	await get_tree().create_timer(0.05).timeout
+	if !is_summoned:
+		get_window().mouse_passthrough_polygon = PackedVector2Array([Vector2(0, 0), Vector2(0, 1)])
--- src/pet.gd
+++ src/pet.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 signal pet_changed
+signal faced_down
 
 enum State { IDLE, ROAM, DRAG, SLEEP, PET, WANDER, ANGRY }
 
-const starting_deck = [ State.IDLE, State.IDLE, State.IDLE, State.ROAM, State.SLEEP, State.SLEEP, State.SLEEP, State.WANDER, State.WANDER ]
+const state_starting_deck = [ State.IDLE, State.IDLE, State.IDLE, State.ROAM, State.SLEEP, State.SLEEP, State.WANDER, State.WANDER ]
+const blink_starting_deck = [ 1, 2, 3, 4 ]
+const invert_shader = preload("res://src/Invert.tres")
 
 @export var speed: float = 100.0
 @export var angle_speed: float = 2.0
@@ -16,13 +19,16 @@ const starting_deck = [ State.IDLE, State.IDLE, State.IDLE, State.ROAM, State.SL
 var state = State.IDLE
 var last_state = State.IDLE
 var direction = Vector2.ZERO
+var prev_direction = Vector2.ZERO
 var wander_direction = Vector2.DOWN
 var drag_last_sprite_pos = Vector2.ZERO
 var drag_last_mouse_pos = Vector2.ZERO
 var angle = 0.0
 var delta_sum = 0.0
+var blink_countdown = 2
 
-var state_deck = [ State.IDLE, State.IDLE, State.IDLE, State.ROAM, State.SLEEP, State.SLEEP, State.SLEEP, State.WANDER, State.WANDER ]
+var state_deck = []
+var blink_deck = []
 
 func set_is_polygon_visible(value):
 	is_polygon_visible = value
@@ -47,9 +53,16 @@ func set_reset_polygon(_value):
 func get_reset_polygon():
 	return reset_polygon
 
+func toggle_inverted():
+	if !$Sprite.material:
+		$Sprite.material = invert_shader
+		$StackedSprite.shader = invert_shader
+	else:
+		$Sprite.material = null
+		$StackedSprite.shader = null
+
 func _ready():
 	randomize()
-	state_deck.shuffle()
 	change_state(State.IDLE)
 	set_is_polygon_visible(is_polygon_visible)
 
@@ -59,17 +72,26 @@ func _process(delta):
 	handle_debug_inputs()
 	handle_state(delta)
 	handle_y_offset()
+	if $Button.get_global_rect().encloses(Rect2(get_global_mouse_position(), Vector2.ZERO)) and Input.is_action_just_pressed("right_click"):
+		get_tree().quit()
 	if $Button.button_pressed and has_dragged_sprite():
 		if state != State.DRAG:
+			setup_polygon(true)
 			change_state(State.DRAG)
 		position = drag_last_sprite_pos + (get_global_mouse_position() - drag_last_mouse_pos)
 
 func pop_state_deck():
 	if state_deck.is_empty():
-		state_deck = starting_deck.duplicate()
+		state_deck = state_starting_deck.duplicate()
 		state_deck.shuffle()
 	var next_state = state_deck.pop_front()
 	return next_state
+
+func peek_state_deck():
+	if state_deck.is_empty():
+		state_deck = state_starting_deck.duplicate()
+		state_deck.shuffle()
+	return state_deck.front()
 
 func handle_y_offset():
 	if $AnimationPlayer.current_animation == "bob_up_down":
@@ -98,21 +120,33 @@ func handle_debug_inputs():
 
 func handle_state(delta):
 	delta_sum += delta
+	prev_direction = direction
 	match state:
 		State.ROAM:
 			angle += delta * angle_speed
-			direction = Vector2(cos(angle), sin(angle))
+			if last_state == State.WANDER or last_state == State.ROAM:
+				direction = lerp(direction, Vector2(cos(angle), sin(angle)), 1.0 * delta)
+			else:
+				direction = Vector2(cos(angle), sin(angle))
 			position += direction * speed * delta
 			$StackedSprite.set_sprite_rotation(direction.angle() + deg_to_rad(-90))
+			$StackedSprite.show(); $Sprite.hide();
+			var check_angle = Vector2.DOWN.rotated(deg_to_rad(3.5)).angle()
+			if prev_direction.angle() < check_angle and direction.angle() > check_angle:
+				faced_down.emit()
 		State.WANDER:
 			if is_out_of_bounds() and $WanderRotateTimer.is_stopped():
 				wander_direction = wander_direction.rotated(PI)
 				$WanderRotateTimer.start()
-			direction = wander_direction + Vector2(sin(delta_sum), sin(delta_sum))
+			direction = lerp(direction, wander_direction + Vector2(sin(delta_sum), sin(delta_sum)), 1.0 * delta)
 			position += direction * 7.5 * delta
 			$StackedSprite.set_sprite_rotation(direction.angle() + deg_to_rad(-90))
-	position.x = clamp(position.x, 8, get_viewport_rect().size.x - 8)
-	position.y = clamp(position.y, 16, get_viewport_rect().size.y - 4)
+			$StackedSprite.show(); $Sprite.hide();
+	position.x = clamp(position.x, 12, get_viewport_rect().size.x - 12)
+	position.y = clamp(position.y, 24, get_viewport_rect().size.y - 4)
+
+func is_roughly_approx(vec_one, vec_two):
+	return abs(vec_one.x - vec_two.x) < 0.05 and abs(vec_one.y - vec_two.y) < 0.05
 
 func is_out_of_bounds():
 	if position.x < 24 or position.x > get_viewport_rect().size.x - 24:
@@ -122,14 +156,23 @@ func is_out_of_bounds():
 	return false
 
 func change_state(next_state):
-	$StackedSprite.visible = next_state == State.ROAM or next_state == State.WANDER
-	$Sprite.visible = next_state != State.ROAM and next_state != State.WANDER
+	if state != State.DRAG and state != State.PET:
+		last_state = state
+	state = next_state
+	if next_state != State.ROAM and next_state != State.WANDER:
+		$Sprite.show()
+		$StackedSprite.visible = false
+	$StateChangeTimer.wait_time = randf_range(4, 8)
 	match next_state:
 		State.IDLE:
 			$AnimationPlayer.play("idle")
 		State.ROAM:
+			if last_state != State.ROAM and last_state != State.WANDER:
+				angle = Vector2.DOWN.angle()
 			$AnimationPlayer.play("bob_up_down")
 		State.WANDER:
+			if last_state != State.ROAM and last_state != State.WANDER:
+				direction = Vector2.DOWN
 			if randi() % 3 == 0:
 				wander_direction = wander_direction.rotated(PI)
 			$AnimationPlayer.play("bob_up_down")
@@ -137,55 +180,69 @@ func change_state(next_state):
 			$StateChangeTimer.paused = true
 			$AnimationPlayer.play("drag")
 		State.SLEEP:
-			$AnimationPlayer.play("sleep")
+			$AnimationPlayer.play("sleep_intro")
+			$AnimationPlayer.queue("sleep_loop")
 		State.PET:
 			$StateChangeTimer.paused = true
 			$AnimationPlayer.play("pet")
 		State.ANGRY:
 			$StateChangeTimer.paused = true
 			$AnimationPlayer.play("angry")
-	if state != State.DRAG and state != State.PET:
-		last_state = state
-	state = next_state
 
-func setup_polygon():
+var next_polygon = PackedVector2Array([])
+func setup_polygon(is_dragging=false):
 	var stacked_sprite = find_child("StackedSprite")
 	var first_child = stacked_sprite.get_child(0)
 	var last_child = stacked_sprite.get_child(stacked_sprite.get_child_count() - 1)
 	var sprite_width = last_child.texture.get_width() / last_child.hframes
 	var half_width = sprite_width / 2.0
-	var next_polygon = PackedVector2Array([last_child.position - Vector2(half_width, sprite_width), last_child.position + Vector2(sprite_width + half_width, -sprite_width),
-	first_child.position + Vector2(sprite_width + half_width, sprite_width), first_child.position + Vector2(-half_width, sprite_width)])
+	if is_dragging:
+		next_polygon = PackedVector2Array([last_child.position - Vector2(half_width, sprite_width), last_child.position + Vector2(sprite_width + half_width, -sprite_width),
+			first_child.position + Vector2(sprite_width + half_width, sprite_width), first_child.position + Vector2(-half_width, sprite_width)])
+	else:
+		next_polygon = PackedVector2Array([last_child.position - Vector2(half_width / 3.0, half_width), last_child.position + Vector2(sprite_width + half_width / 3.0, -half_width),
+			first_child.position + Vector2(sprite_width + half_width / 3.0, half_width), first_child.position + Vector2(-half_width / 3.0, half_width)])
 	for i in range(0, len(next_polygon)):
 		next_polygon[i].x -= half_width
 	$Polygon2D.polygon = next_polygon
 
 func _on_button_down():
+	$UpdateTimer.wait_time = 0.025
+	$UpdateTimer.start()
 	drag_last_mouse_pos = get_global_mouse_position()
 	drag_last_sprite_pos = position
 
 func _on_button_button_up():
+	$UpdateTimer.wait_time = 0.05
+	$UpdateTimer.start()
 	if state == State.DRAG:
+		setup_polygon(false)
 		$StateChangeTimer.paused = false
-		if last_state == State.SLEEP:
-			change_state(State.IDLE)
-		else:
-			change_state(last_state)
+		change_state(State.IDLE)
 	elif !has_dragged_sprite() and !state == State.PET:
 		change_state(State.PET)
 
 func _on_state_change_timer_timeout():
+	var next_state = peek_state_deck()
 	match state:
 		State.IDLE:
-			$StateChangeTimer.wait_time = randf_range(6, 10)
+			await $AnimationPlayer.animation_finished
 		State.ROAM:
-			$StateChangeTimer.wait_time = randf_range(2, 6)
-		State.SLEEP:
-			$StateChangeTimer.wait_time = randf_range(6, 10)
+			if next_state != State.ROAM and next_state != State.WANDER:
+				$FaceDownTimeoutTimer.start()
+				await faced_down
+				$FaceDownTimeoutTimer.stop()
 		State.WANDER:
-			$StateChangeTimer.wait_time = randf_range(2, 6)
-		_:
-			$StateChangeTimer.wait_time = randf_range(6, 8)
+			if next_state != State.ROAM and next_state != State.WANDER:
+				$FaceDownTimeoutTimer.start()
+				change_state(State.ROAM)
+				await faced_down
+				$FaceDownTimeoutTimer.stop()
+		State.SLEEP:
+			if $AnimationPlayer.current_animation == "sleep_loop":
+				await $AnimationPlayer.animation_finished
+			$AnimationPlayer.play("sleep_outro")
+			await $AnimationPlayer.animation_finished
 	change_state(pop_state_deck())
 	$StateChangeTimer.start()
 
@@ -193,9 +250,26 @@ func _on_update_timer_timeout():
 	pet_changed.emit()
 
 func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "sleep_loop":
+		$AnimationPlayer.play("sleep_loop")
 	if anim_name == "pet":
 		$StateChangeTimer.paused = false
 		if last_state == State.SLEEP:
 			change_state(State.IDLE)
 		else:
 			change_state(last_state)
+	if anim_name == "idle":
+		blink_countdown -= 1
+		if blink_countdown <= 0:
+			if blink_deck.is_empty():
+				blink_deck = blink_starting_deck.duplicate()
+				blink_deck.shuffle()
+			blink_countdown = blink_deck.pop_front()
+			$AnimationPlayer.play("idle_blink")
+		else:
+			$AnimationPlayer.play("idle")
+	if anim_name == "idle_blink":
+		$AnimationPlayer.play("idle")
+
+func _on_face_down_timeout_timer_timeout():
+	faced_down.emit()
